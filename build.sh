5a33c9b000f90bf8c145ef30f015d66489d9d56e
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-08-31 10:55:42 +0530

    Build

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ docker pull tibdocker.tibco.com/mashling/mashling-sample:master
 docker run -it -p 9096:9096 tibdocker.tibco.com/mashling/mashling-sample:master
 fi
 
-curl -I -X GET http://localhost:9096/pets/2
-curl -I -X PUT -d '{"name":"CAT"}' http://localhost:9096/pets
+#curl -I -X GET http://localhost:9096/pets/2
+#curl -I -X PUT -d '{"name":"CAT"}' http://localhost:9096/pets
 curl -I -X PUT -d '{"name":"CATs"}' http://localhost:9096/pets
