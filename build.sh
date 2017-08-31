935a9df6e9a2e5851c70b0330fc244d17544f4ba
Lakshmi Narayana Mekala <lmekala@tibco.com>
2017-08-31 10:51:29 +0530

    Build

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -n "${TRAVIS_TAG}" ]; then
+docker pull tibdocker.tibco.com/mashling/mashling-sample:latest
+docker run -it -p 9096:9096 tibdocker.tibco.com/mashling/mashling-sample:latest
+fi
+
+if [ -z "${TRAVIS_TAG}" ]; then
+docker pull tibdocker.tibco.com/mashling/mashling-sample:master
+docker run -it -p 9096:9096 tibdocker.tibco.com/mashling/mashling-sample:master
+fi
+
+curl -I -X GET http://localhost:9096/pets/2
+curl -I -X PUT -d '{"name":"CAT"}' http://localhost:9096/pets
+curl -I -X PUT -d '{"name":"CATs"}' http://localhost:9096/pets
