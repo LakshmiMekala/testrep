a8bdcc2aebf01d3c9623edf0107a0ec8769f3434
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-01-23 20:16:53 +0530

    version fix 5

--- secure.sh
+++ secure.sh
@@ -53,7 +53,7 @@ function get_test_cases {
 	input="{"Hobbies":["snowboarding","programming"],"Name":"Alex"}"
 	#output=Hobbies
 	
-	if [[ "echo $(cat /tmp/client.log)" =~ "echo $(/tmp/gw.log)" ]] 
+	if [[ "echo $(cat /tmp/client.log)" =~ "echo $(cat /tmp/gw.log)" ]] 
         then 
             echo "PASS"
             
@@ -64,6 +64,7 @@ function get_test_cases {
 	
 	popd
 	
+	rm -f /tmp/client.log /tmp/gw.log
 	kill -SIGINT $pId
 	kill -SIGINT $pId1
 	kill -SIGINT $pId2
