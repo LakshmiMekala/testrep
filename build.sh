8ee2f1740ef14f238f766b4e5f4994beb915c803
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-01-29 14:16:54 +0530

    installing pre requisites

--- build.sh
+++ build.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 ##Installing pre requisites to run performance test
-   sudo aptget install zookeeperd
-   sudo aptget update q
+   sudo apt-get install zookeeperd
+   sudo apt-get update q
    cd $GOPATH
    wget http://wwweu.apache.org/dist/kafka/1.0.0/kafka_2.111.0.0.tgz O $GOPATH/kafka.tgz
    mkdir p $GOPATH/kafka && cd $GOPATH/kafka
    tar xvzf $GOPATH/kafka.tgz strip 1 > /tmp/kafka.log
    cd $GOPATH
-   wget https://sourceforge.net/projects/grinder/files/latest/download $GOPATH/grinder.zip
-   mkdir p $GOPATH/grinder && cd $GOPATH/grinder
-   unzip $GOPATH/grinder.zip strip 1 > /tmp/grinder.log
-   cd $GOPATH 
-   touch setGrinderEnv.sh
-   echo "#!/usr/bin/ksh
-        GRINDERPATH=$GOPATH/grinder
-        GRINDERPROPERTIES=<em>(full path to grinder.properties)</em>/grinder.properties
-        CLASSPATH=$GRINDERPATH/lib/grinder.jar:$CLASSPATH
-        JAVA_HOME=<em>(full path to java installation directory)</em>
-        PATH=$JAVA_HOME/bin:$PATH
-        export CLASSPATH PATH GRINDERPROPERTIES" >> $GOPATH/setGrinderEnv.sh
+   wget https://sourceforge.net/projects/grinder/files/latest/download $GOPATH/download.zip
+   unzip $GOPATH/download > /tmp/grinder.log
+   rm -f download
+   mv grinder-* grinder
+   #setting grinder paths
+    GRINDERPATH=$GOPATH/grinder
+    GRINDERPROPERTIES=$GOPATH/grinder/examples/grinder.properties
+    CLASSPATH=$GRINDERPATH/lib/grinder.jar:$CLASSPATH
+    JAVA_HOME=$JAVA_HOME
+    PATH=$JAVA_HOME/bin:$PATH
+    export CLASSPATH PATH GRINDERPROPERTIES
+    #    java -classpath $CLASSPATH net.grinder.Grinder $GRINDERPROPERTIES
+    #    java -classpath $CLASSPATH net.grinder.Console
+    
+    # Installing multi mechanize
+    sudo apt-get install python-pip python-matplotlib
+    sudo pip install -U multi-mechanize
