084f74ecd84bb6c2e465203c9b524169fa043062
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-01-30 15:57:39 +0530

    final test -check v0.3

--- pre-requisites.sh
+++ pre-requisites.sh
@@ -4,7 +4,6 @@
    sudo apt-get install zookeeperd
    sudo apt-get install libxml2-utils
    sudo pip install kafka-python
-   sudo pip install tox-travis
    sudo apt-get update -q
    pushd $GOPATH   
    wget http://www-eu.apache.org/dist/kafka/1.0.0/kafka_2.11-1.0.0.tgz -O $GOPATH/kafka.tgz
