a8f343274a1c9cc3d68537b177d3499fedf17fef
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-02-12 19:27:11 +0530

    checking with old version 186 builds

--- NewPerformance.sh
+++ NewPerformance.sh
@@ -17,7 +17,7 @@ echo $HTML >> $GOPATH/$FILENAME
 
 #Kafka Performance
 cd $GOPATH/src/github.com/LakshmiMekala/testrep/
-source ./kafka.sh
+source ./kafka-186.sh
 value=($(get_test_cases))
 for ((i=0;i < ${#value[@]};i++))
         do
