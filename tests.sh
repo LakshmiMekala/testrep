f54836a23b8dadeb69192b4909524910e9b8e405
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-02-22 14:33:14 +0530

    revert test commit

--- tests.sh
+++ tests.sh
@@ -36,18 +36,18 @@ function testcase1 {
 	cd ..
 	multimech-run my_project &
 	pId3=$!	
-	sleep 200
+	 sleep 200
 	echo pid3=$pId3
-	var=$(ps --ppid $pId3)
-	echo var=$var
-	pId4=$(echo $var | awk '{print $5}')
-	echo 4=$pId4
-	echo completed
-	sleep 10
-	kill -9 $pId4
+	# var=$(ps --ppid $pId3)
+	# echo var=$var
+	# pId4=$(echo $var | awk '{print $5}')
+	# echo 4=$pId4
+	# echo completed
+	# sleep 10
+	# kill -9 $pId4
 	#echo var=$var
 	sleep 40
-	# kill -s TERM $pId3
+	kill -s TERM $pId3
 	kill -SIGINT $pId
 	kill -s TERM $pId
 	sleep 5
