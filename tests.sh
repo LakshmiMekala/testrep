c31631a3fa999c636fd59e163722be2d154ce39e
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-02-23 10:18:09 +0530

    run re build

--- tests.sh
+++ tests.sh
@@ -25,8 +25,8 @@ function testcase1 {
 	pId2=$!
 	sleep 10
 
-	testTime=1800
-	Threads=100
+	testTime=180
+	Threads=10
 	#var="$(timeout 70s multimech-run my_project &)"
 	echo started
 	cd $GOPATH/src/github.com/LakshmiMekala/testrep/KafkaTrigger-To-KafkaPublisher/my_project
@@ -36,7 +36,7 @@ function testcase1 {
 	cd ..
 	multimech-run my_project &
 	pId3=$!	
-	sleep 2500
+	 sleep 200
 	echo pid3=$pId3
 	# var=$(ps --ppid $pId3)
 	# echo var=$var
@@ -45,8 +45,8 @@ function testcase1 {
 	# echo completed
 	# sleep 10
 	# kill -9 $pId4
-	# echo var=$var
-	# sleep 40
+	#echo var=$var
+	sleep 40
 	kill -s TERM $pId3
 	kill -SIGINT $pId
 	kill -s TERM $pId
@@ -103,7 +103,7 @@ function testcase2 {
 	export FLOGO_LOG_LEVEL=ERROR
 	./kafkatrigger-to-kafkapublisher > /tmp/kafka-testcase2.log &
 	pId2=$!
-	sleep 30
+	sleep 10
 
 	testTime=10
 	#var="$(timeout 70s multimech-run my_project &)"
@@ -114,15 +114,6 @@ function testcase2 {
 	multimech-run my_project & pId3=$!
 	echo completed
 	sleep 60
-	echo pid3=$pId3
-	var=$(ps --ppid $pId3)
-	echo var=$var
-	pId4=$(echo $var | awk '{print $5}')
-	echo 4=$pId4
-	echo completed
-	sleep 10
-	kill -9 $pId4
-	echo var=$var
 	#echo var=$var
 	kill -s TERM $pId3
 	kill -SIGINT $pId
